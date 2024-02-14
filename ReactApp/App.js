337a105a9711e5fed79bcc343a6dd8a8d1b4efd1
ValCordDev <88210628+ValCordDev@users.noreply.github.com>
2024-02-14 19:20:07 +0100

    created simple project

---
+++ ReactApp/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 bg-[#232323] items-center justify-center'>
+      <Text className='text-3xl font-bold text-white'>Hey!</Text>
+      <Text className='text-lg text-gray-400'>Welcome to React Native</Text>
+      <Button title='Click me' onPress={() => alert('Button clicked!')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
